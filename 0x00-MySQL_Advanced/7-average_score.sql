214ca87e6b6e11cf59289e80229fec43e226a8e6
zaynom <ouaziz0626@gmail.com>
2024-04-25 21:57:08 +0000

    task 7: Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,7 +7,7 @@ BEGIN
 	UPDATE users SET average_score = (
 		SELECT AVG(score) 
 		FROM corrections
-		WHERE user_id = user_id)
+		WHERE corrections.user_id = user_id)
 	WHERE id = user_id;
 END;
 //
