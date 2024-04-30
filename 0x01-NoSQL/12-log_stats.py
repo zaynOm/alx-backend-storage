6f31eb094cacaeb62b3e2da98d4ebac133b59080
zaynom <ouaziz0626@gmail.com>
2024-04-30 08:31:16 +0100

    task 12: Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 "Log stats"
 
-from itertools import zip_longest
-from pymongo import MongoClient
+if __name__ == "__main__":
+    from itertools import zip_longest
+    from pymongo import MongoClient
 
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-nginx_collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{nginx_collection.count_documents({})} logs")
+    print("Methods:")
+    res = nginx_collection.aggregate(
+        [
+            {"$match": {"method": {"$in": methods}}},
+            {"$group": {"_id": "$method", "count": {"$count": {}}}},
+        ]
+    )
 
-print(f"{nginx_collection.count_documents({})} logs")
-print("Methods:")
-res = nginx_collection.aggregate(
-    [
-        {"$match": {"method": {"$in": methods}}},
-        {"$group": {"_id": "$method", "count": {"$count": {}}}},
-    ]
-)
+    for method, doc in zip_longest(methods, res):
+        if doc:
+            print(f"    {method}: {doc.get('count')}")
+        else:
+            print(f"    {method}: 0")
 
-for method, doc in zip_longest(methods, res):
-    if doc:
-        print(f"    {method}: {doc.get('count')}")
-    else:
-        print(f"    {method}: 0")
-
-status_count = nginx_collection.count_documents(
-    {"method": "GET", "path": "/status"}
-)
-print(f"{status_count} status check", end="")
+    status_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_count} status check", end="")
